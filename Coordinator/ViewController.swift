96aee586b114c18bcb02b7f9ba1bb67586c6673f
Vitor Venturin <vitorvl@yahoo.com.br>
2021-01-08 13:21:01 +0100

    Initial Commit

---
+++ Coordinator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coordinator
+//
+//  Created by Vitor Venturin on 1/8/21.
+//  Copyright © 2021 Vitor Venturin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
